efdf7a8944dd5dead2926de331b2889dac8b57e0
Wan Zulsarhan <zulsarhan.shaari@gmail.com>
2018-04-12 16:30:32 +0800

    first

---
+++ lib/private_template.php
@@ -0,0 +1,10 @@
+<?php
+
+define("DB_HOST", "");
+define("DB_NAME", "");
+define("DB_USER", "");
+define("DB_PASSWORD", "");
+
+define("EMAIL_FROM","");
+define("EMAIL_NAME","");
+define("EMAIL_PASSWORD","");
