da040f924fa7795d8c3c147a7adcaa970ed9b724
es021 <zulsarhan.shaari@gmail.com>
2019-06-14 13:15:54 +0800

    A

--- delete-daily-co-room.php
+++ delete-daily-co-room.php
@@ -1,8 +1,14 @@
 <?php
+
+// delete room daily co
+// masuk dalam table baru, yang dh deleted 
+//     ps_daily_co_deleted {ID, pre_screens_id, room_name, created_at}
+// update prescreen status kepada 4_Ended
+
 include_once 'lib/DB.php';
 
 $DB = new DB();
-$q = "select p.* from pre_screens p where p.join_url like '%seedsjobfair.daily.co%' ";
+$q = "SELECT p.* FROM pre_screens p WHERE p.join_url LIKE '%seedsjobfair.daily.co%' ";
 $data = $DB->query_array($q);
 
 foreach ($data as $d) {
@@ -14,5 +20,7 @@ foreach ($data as $d) {
     $roomName = $arr[1];
     X($roomName);
 }
+
+$sql = "INSERT INTO ps_daily_co_deleted () VALUES ()";
 
 ?>
