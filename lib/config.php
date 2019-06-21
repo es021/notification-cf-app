868b3c72a8547328997c14d3e970fc5274beb9ea
es021 <zulsarhan.shaari@gmail.com>
2019-06-21 11:37:31 +0800

    A

---
+++ lib/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("APP_URL", "http://seedsjobfairapp.com/cf/app");
+define("APP_AUTH_URL", "http://seedsjobfairapp.com/cf/auth");
+define("APP_NAME", "SeedsJobFairApp.com");
+define("CONVERT_UNIX_URL", "http://www.convert-unix-time.com/?t=");
+
+?>
